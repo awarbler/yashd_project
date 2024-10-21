6454957690826453ae149e6d156fddacfd48a0f4
Anita Woodford <awoodfor@byui.edu>
2024-10-21 09:51:32 -0500

    Update yashd.h
    correct spelling

--- yashd.h
+++ yashd.h
@@ -1,17 +1,17 @@
 // this is the header file yashd.h
 
-// include a guard to prevent mutiple inclusions of the header file
+// include a guard to prevent multiple inclusions of the header file
 
 #ifndef YASHD_H // If yashd_h is not defined
 #define YASHD_H // define yashd_h
 
-// including standard libraries for various functionalitites for the client
+// including standard libraries for various functionalities for the client
 #include <stdio.h> // standard input output library 
 #include <stdlib.h> // standard library includes functions malloc() free() 
 #include <pthread.h> // pthread header
 #include <netinet/in.h> // for sockets /* inet_addr() */
 #include <arpa/inet.h>      // internet operations inetpton()
-#include <unistd.h>         // posix api for close () 
+#include <unistd.h>         // POSIX api for close () 
 #include <signal.h>         // signal handling sigint sigtstp 
 #include <errno.h>          // error handling macros
 
@@ -33,9 +33,9 @@
 #include <time.h>
 
 // function prototype for handle_client
-// this function is responsible for handling communication with a connected client in a seperate thread 
-// this protoype declares teh function so that it can be used in mutiple source files where the header is included
-// this fullfills the requirement of using a mult threaded server to handle multiple clients concurrently. 
+// This function is responsible for handling communication with a connected client in a separate thread 
+// this prototype declares the function so that it can be used in multiple source files where the header is included
+// This fulfills the requirement of using a multi-threaded server to handle multiple clients concurrently. 
 void *handle_client(void *arg);
 
 #endif // end of the include. 
